85e1b4b4f1136e44a682feadd2b02de1218bca83
Arkadiusz Mochol <mochol_arkadiusz@o2.pl>
2020-07-11 15:59:13 +0200

    AT-1. HibernateUtil, AppUser implemented. Database configuration added.

---
+++ src/main/java/hibernate/util/HibernateUtil.java
@@ -0,0 +1,55 @@
+package hibernate.util;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.*;
+
+public class HibernateUtil {
+    //singelton najprostszy wzorzec
+    private static HibernateUtil instance;
+    private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myDatabase");                      //do komunikacji z DB
+    //
+    private final EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    public static HibernateUtil getInstance() {
+        if (null == instance) {
+            instance = new HibernateUtil();                    //nikt nie wywoływał, wiec tworzymy
+        }
+        return instance;
+    }
+
+    public void saveByHibernateSession(Object o) {
+        try {
+            Session session = entityManager.unwrap(Session.class);
+            Transaction transaction = session.beginTransaction();
+            session.save(o);
+            transaction.commit();
+            session.close();
+        } catch (PersistenceException e) {
+            System.out.println("Could not unwrap session : " +e );
+        }
+    }
+    //jpa
+    public void save(Object o){
+        entityManager.getTransaction().begin();
+        if(!entityManager.contains(o)){
+            entityManager.persist(o);
+            entityManager.flush();
+        }
+        entityManager.getTransaction().commit();
+    }
+    public void delete(Class clazz, Long id){
+        entityManager.getTransaction().begin();
+        Object toRemove = entityManager.find(clazz, id);
+        entityManager.remove(toRemove);
+        entityManager.getTransaction().commit();
+    }
+
+    public EntityManager getEntityManager(){
+        return entityManager;
+    }
+
+
+
+}
